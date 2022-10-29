01b765bc98ff004c7f4480cfb0424328f6f62169
EmilsWebbod <emil@webbod.no>
2022-10-29 22:27:28 +0200

    Try to fix export. Removed some files

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+interface WordTranslations {
+  [key: string]: {
+    [key: string]: string;
+  };
+}
+interface TranslateMinifyOptions {
+  defaultLocale?: string;
+  locale?: string;
+  words?: WordTranslations;
+  texts?: WordTranslations;
+}
+interface Variables {
+  [variable: string]: string | number | undefined;
+}
+interface TextOptions extends Variables {
+  locale?: string;
+}
+export declare const VARIABLE_REGEXP: RegExp;
+export default class TranslateMinify {
+  private readonly defaultLocale;
+  private words;
+  private texts;
+  private locale;
+  constructor(
+    { defaultLocale, locale, words, texts }?: TranslateMinifyOptions,
+    overrideNew?: boolean
+  );
+  w(word: string, locale?: string): string;
+  word(word: string, locale?: string): string;
+  t(text: string, opts?: TextOptions): string;
+  text(text: string, { locale, ...variables }?: TextOptions): string;
+  setLocale(locale: string): void;
+  addWords(newWords: WordTranslations): void;
+  addTexts(newTexts: WordTranslations): void;
+  addWordsTranslations(translations: WordTranslations): void;
+  addTextsTranslations(translations: WordTranslations): void;
+  private replaceVariables;
+}
+export { TranslateMinifyOptions, WordTranslations, TextOptions };
---
+++ index.js
@@ -0,0 +1,111 @@
+'use strict';
+var __rest =
+  (this && this.__rest) ||
+  function (s, e) {
+    var t = {};
+    for (var p in s)
+      if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === 'function')
+      for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+        if (
+          e.indexOf(p[i]) < 0 &&
+          Object.prototype.propertyIsEnumerable.call(s, p[i])
+        )
+          t[p[i]] = s[p[i]];
+      }
+    return t;
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.VARIABLE_REGEXP = void 0;
+exports.VARIABLE_REGEXP = /{{(.*?)}}/g;
+let translate;
+class TranslateMinify {
+  constructor(
+    { defaultLocale = 'en', locale = 'en', words = {}, texts = {} } = {},
+    overrideNew
+  ) {
+    this.defaultLocale = 'en';
+    this.words = {};
+    this.texts = {};
+    this.locale = 'en';
+    if (!overrideNew && translate) {
+      return translate;
+    }
+    this.defaultLocale = defaultLocale;
+    this.locale = locale;
+    this.words = words;
+    this.texts = texts;
+    translate = this;
+  }
+  w(word, locale) {
+    return this.word(word, locale);
+  }
+  word(word, locale = this.locale) {
+    if (this.defaultLocale === locale) {
+      return word;
+    }
+    const wordObj = this.words[word];
+    const translated = wordObj ? wordObj[locale] : word;
+    return translated || word;
+  }
+  t(text, opts) {
+    return this.text(text, opts);
+  }
+  text(text, _a = {}) {
+    var { locale = this.locale } = _a,
+      variables = __rest(_a, ['locale']);
+    if (this.defaultLocale === locale) {
+      return this.replaceVariables(text, variables);
+    }
+    const textObj = this.texts[text];
+    const translated = textObj ? textObj[locale] : text;
+    return this.replaceVariables(translated || text, variables);
+  }
+  setLocale(locale) {
+    this.locale = locale;
+  }
+  addWords(newWords) {
+    this.words = Object.assign(Object.assign({}, this.words), newWords);
+  }
+  addTexts(newTexts) {
+    this.texts = Object.assign(Object.assign({}, this.texts), newTexts);
+  }
+  addWordsTranslations(translations) {
+    for (const key in translations) {
+      if (translations.hasOwnProperty(key) && this.words[key]) {
+        this.words[key] = Object.assign(
+          Object.assign({}, this.words[key]),
+          translations[key]
+        );
+      }
+    }
+  }
+  addTextsTranslations(translations) {
+    for (const key in translations) {
+      if (translations.hasOwnProperty(key) && this.texts[key]) {
+        this.texts[key] = Object.assign(
+          Object.assign({}, this.texts[key]),
+          translations[key]
+        );
+      }
+    }
+  }
+  replaceVariables(text, variables) {
+    if (Object.keys(variables).length === 0) {
+      return text;
+    }
+    return text.replace(exports.VARIABLE_REGEXP, (_word, group) => {
+      var _a;
+      return String(
+        group in variables
+          ? (_a = variables[group]) !== null && _a !== void 0
+            ? _a
+            : ''
+          : ''
+      );
+    });
+  }
+}
+exports.default = TranslateMinify;
+//# sourceMappingURL=index.js.map
